810a9a3a62cbccb8f0fab1b54dcf9d9f373b4531
Bingyu Lin <bylin@thoughtworks.com>
2015-02-11 17:57:22 +0800

    bylin: add method to send email to santa

--- quiz2_secret_santa/santa.rb
+++ quiz2_secret_santa/santa.rb
@@ -1,3 +1,5 @@
+require 'net/smtp'
+
 class Santa
   def initialize(name_list)
     @name_list = name_list
@@ -23,9 +25,34 @@ class Santa
           end
         end
       end
-
+      if(@santa_hash[@name_list[index_i]] == nil)
+        @santa_hash[@name_list[index_i]] = 'No person available.'
+      end
     end
     @santa_hash
+  end
+
+  def email_santa(santa_hash, from, password)
+    to = from
+    smtp_host = "smtp.gmail.com"
+    subject = "Secret Santa"
+
+    santa_hash.each_pair { |k, v|
+      smtp = Net::SMTP.new smtp_host, 587
+      smtp.enable_starttls
+
+      santa = k.split(' ')
+      person = v.split(' ')
+      #to = santa[2].gsub('<', '').gsub('>', '')
+      msg = "Hi " + santa[0] + ' ' + santa[1] + ",\r\n\r\n    Your person is: " + person[0] + ' ' + person[1] + '!'
+      mail = "To: #{to}\r\n" + "From: #{from}\r\n" + "Subject: #{subject}\r\n" + "\r\n" + msg
+
+      smtp.start('gmail.com', from, password, :plain) do
+        smtp.send_mail(mail, from, to)
+        #smtp.send_message(msg, from, to)
+      end
+    }
+
   end
 
 end
